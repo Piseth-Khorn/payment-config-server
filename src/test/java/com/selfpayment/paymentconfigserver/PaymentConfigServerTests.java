a471a09096d8779703dcedb3ad7df0f09ad5dac9
Piseth KHON <piseth.khon@presta.tessi.fr>
2023-07-11 16:18:23 +0700

    Intial project

---
+++ src/test/java/com/selfpayment/paymentconfigserver/PaymentConfigServerTests.java
@@ -0,0 +1,13 @@
+package com.selfpayment.paymentconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentConfigServerTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
